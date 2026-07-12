aa5bc3b53aff28c9065d13903b51741b582081d7
Roshan Kumar <roshankum1507@gmail.com>
2026-07-12 06:54:44 +0530

    Time: 1 ms (98.7%), Space: 47.4 MB (59.92%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -3,57 +3,25 @@ import java.util.*;
 class Solution {
     public int pivotIndex(int[] nums) {
 
-        Map<Integer, Integer> map1 = new HashMap<>();
+        int total = 0;
 
-        int sum1 = 0;
+        for (int x : nums) {
+
+            total += x;
+        }
+
+        int leftsum = 0;
+
         for (int i = 0; i < nums.length; i++) {
 
-            sum1 += nums[i];
+            int rightsum = total - leftsum - nums[i];
 
-            map1.put(i, sum1);
-        }
-
-        Map<Integer, Integer> map2 = new HashMap<>();
-        int sum2 = 0;
-        for (int j = nums.length - 1; j >= 0; j--) {
-
-            sum2 += nums[j];
-
-            map2.put(j, sum2);
-
-        }
-
-        for(int i = 0 ; i<nums.length ; i++){
-            
-            int left= 0;
-            if(i==0){
-
-                left =0 ;
-            }
-
-            else{
-
-                left = map1.get(i-1);
-            }
-
-            int right;
-
-            if(i == nums.length-1){
-
-                right =0;
-
-            }
-
-            else{
-
-                right = map2.get(i+1);
-            }
-
-            if(left == right){
+            if (rightsum == leftsum) {
 
                 return i;
-
             }
+
+            leftsum += nums[i];
         }
 
         return -1;
