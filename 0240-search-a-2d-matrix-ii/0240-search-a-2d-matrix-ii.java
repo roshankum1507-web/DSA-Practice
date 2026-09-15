bcf683ac9c38c4a843c8c24ba26ddd8d4111acb8
Roshan Kumar <roshankum1507@gmail.com>
2026-09-15 21:36:35 +0530

    Time: 3 ms (85.25%), Space: 48.1 MB (64.98%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -1,54 +1,20 @@
 class Solution {
 
-    public boolean binarysearch(int[] matrix, int target) {
-
-        int n = matrix.length;
-        int low = 0;
-        int high = n - 1;
-
-        while (low <= high) {
-
-            int mid = (low + high) / 2;
-
-            if (matrix[mid] == target) {
-
-                return true;
-            }
-
-            else if (matrix[mid] > target) {
-
-                high = mid - 1;
-            }
-
-            else {
-
-                low = mid + 1;
-            }
-
-        }
-
-        return false;
-    }
-
     public boolean searchMatrix(int[][] matrix, int target) {
 
         int m = matrix.length;
         int n = matrix[0].length;
-        boolean result = false;
 
-        for (int i = 0; i < m; i++) {
+        int low = 0 , high = n-1;
+       
 
-            if (matrix[i][0] <= target && matrix[i][n - 1] >= target) {
+        while(low<m && high>=0){
 
-                result = binarysearch(matrix[i], target);
-                if (result == false) {
+            if(matrix[low][high]==target) return true;
 
-                    continue;
-                }
+            else if(matrix[low][high] < target) low++;
 
-                return result;
-            }
-
+            else high--;
         }
 
         return false;
